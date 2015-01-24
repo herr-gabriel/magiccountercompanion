39fbe426643289b189893e6e4634ddcc9f101b7b
Gabriel Shahzad <bbchunkwol@googlemail.com>
2015-01-25 00:20:25 +0100

    Forgot to add setItem to functions.js

--- functions.js
+++ functions.js
@@ -20,46 +20,56 @@ $(document).ready(function(){
 	addOneLife = function() {
 		lifes = parseInt(lifes) + 1;
 		document.getElementById("lifecounter").innerHTML = lifes;
+		localStorage.setItem("lifes", lifes);
 	}
 	addFiveLife = function() {
 		lifes = parseInt(lifes) + 5;
 		document.getElementById("lifecounter").innerHTML = lifes;
+		localStorage.setItem("lifes", lifes);
 	}
 	addTenLife = function() {
 		lifes = parseInt(lifes) + 10;
 		document.getElementById("lifecounter").innerHTML = lifes;
+		localStorage.setItem("lifes", lifes);
 	}
 
 	remOneLife = function() {
 		lifes = parseInt(lifes) - 1;
 		document.getElementById("lifecounter").innerHTML = lifes;
+		localStorage.setItem("lifes", lifes);
 	}
 	remFiveLife = function() {
 		lifes = parseInt(lifes) - 5;
 		document.getElementById("lifecounter").innerHTML = lifes;
+		localStorage.setItem("lifes", lifes);
 	}
 	remTenLife = function() {
 		lifes = parseInt(lifes) - 10;
 		document.getElementById("lifecounter").innerHTML = lifes;
+		localStorage.setItem("lifes", lifes);
 	}
 
 
 	addOnePoison = function() {
 		poisons = parseInt(poisons) + 1;
 		document.getElementById("poisoncounter").innerHTML = poisons;
+		localStorage.setItem("poisons", poisons);
 	}
 	addFivePoison = function() {
 		poisons = parseInt(poisons) + 5;
 		document.getElementById("poisoncounter").innerHTML = poisons;
+		localStorage.setItem("poisons", poisons);
 	}
 
 	remOnePoison = function() {
 		poisons = parseInt(poisons) - 1;
 		document.getElementById("poisoncounter").innerHTML = poisons;
+		localStorage.setItem("poisons", poisons);
 	}
 	remFivePoison = function() {
 		poisons = parseInt(poisons) - 5;
 		document.getElementById("poisoncounter").innerHTML = poisons;
+		localStorage.setItem("poisons", poisons);
 	}
 
 	reset = function() {
